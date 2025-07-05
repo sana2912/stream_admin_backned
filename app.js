31dca7714d7f83bbd28eea4cfdc5883205a0eb9e
sana2912 <path25230@gmail.com>
2025-07-05 23:15:32 +0700

    setting cors for render testing

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors());
+// {
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+// })
 
 connnect_database();
 
